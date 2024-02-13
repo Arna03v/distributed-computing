b37db3aec39198ecc50fe0d9d17d8fb39298229f
= <=>
2024-02-13 18:05:54 +0530

    working with a smaller test file

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -9,17 +9,31 @@
 using namespace std;
 
 std::mutex mtx;
+struct LineData {
+    double x;
+    double pi_x;
+    double delta_x;
+};
 
-int searchInChunk(const vector<std::string>& chunk, const double value, int threadId) {
+int searchInChunk(const vector<string>& chunk, const double value, int threadId) {
     int lineNumber = 0;
     double piValue = -1; // Initialize piValue to -1
-    int nextValue = 0;
+    double nextValue = 0;
     bool found = false;
 
+    // for(auto& line : chunk) cout << line << endl;    the input is correct
+    
     for (const auto& line : chunk) {
+        // initialize a struct LineData for each line
+        
         lineNumber++;
-        std::istringstream iss(line);
+        istringstream iss(line);
         double currentValue;
+
+        if(!(iss >> currentValue >> nextValue)){
+            return -1;
+        }
+        
         iss >> currentValue;
         if (currentValue == value) {
             if (iss >> nextValue) {
@@ -43,7 +57,7 @@ int searchInChunk(const vector<std::string>& chunk, const double value, int thre
 }
 
 int main(int argc, char* argv[]) {
-    std::string filename = "master_file.txt";
+    std::string filename = "test.txt";
 
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " <number_of_chunks> <value_to_search>" << std::endl;
@@ -51,26 +65,28 @@ int main(int argc, char* argv[]) {
     }
 
     int number_of_chunks = std::stoi(argv[1]);
-    int value_to_search = std::stoi(argv[2]);
+    double value_to_search = std::stoi(argv[2]);
 
     std::ifstream file(filename);
     std::string line;
     std::vector<std::string> file_chunks;
 
-    // Read the file and divide it into chunks
+    // Read the file and storing it into a vector of strings
     while (std::getline(file, line)) {
         file_chunks.push_back(line);
     }
     file.close();
 
+    // chunk size 
     int chunk_size = std::ceil(file_chunks.size() / static_cast<double>(number_of_chunks));
 
     std::vector<std::thread> threads;
 
+    // runinng the threads
     for (int i = 0; i < number_of_chunks; i++) {
         int start_index = i * chunk_size;
         int end_index = std::min((i + 1) * chunk_size, static_cast<int>(file_chunks.size()));
-        std::vector<std::string> chunk(file_chunks.begin() + start_index, file_chunks.begin() + end_index);
+        vector<string> chunk(file_chunks.begin() + start_index, file_chunks.begin() + end_index);
         threads.emplace_back(searchInChunk, chunk, value_to_search, i);
     }
 
